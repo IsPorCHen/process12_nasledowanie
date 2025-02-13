f87b7e79e73b3a1bfa58118520762243083eda0b
IsPorCHen <fazy2019@gmail.com>
2025-02-13 23:11:17 +0500

    hello

---
+++ doch_process/doch_process.cpp
@@ -0,0 +1,38 @@
+﻿#include <windows.h>
+#include <iostream>
+
+int wmain(int argc, wchar_t* argv[]) {
+    setlocale(0, "rus");
+    if (argc < 2) {
+        std::wcerr << L"Ошибка: не передан путь к файлу\n";
+        return 1;
+    }
+
+    std::wcout << L"Открываем файл: " << argv[1] << L"\n";
+
+    HANDLE hFile = CreateFile(
+        argv[1], GENERIC_READ, FILE_SHARE_READ,
+        NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL
+    );
+
+    if (hFile == INVALID_HANDLE_VALUE) {
+        std::wcerr << L"Ошибка открытия файла: " << GetLastError() << L"\n";
+        return 1;
+    }
+
+    // Читаем файл
+    char buffer[256] = { 0 };  // Заполняем буфер нулями
+    DWORD read = 0;
+    BOOL success = ReadFile(hFile, buffer, sizeof(buffer) - 1, &read, NULL);
+
+    if (success && read > 0) {
+        buffer[read] = '\0';  // Гарантируем корректное завершение строки
+        std::wcout << L"Содержимое файла: " << buffer << L"\n";
+    }
+    else {
+        std::wcerr << L"Ошибка чтения файла: " << GetLastError() << L"\n";
+    }
+
+    CloseHandle(hFile);
+    return 0;
+}
---
+++ rod_process/rod_process.cpp
@@ -0,0 +1,46 @@
+﻿#include <windows.h>
+#include <iostream>
+
+int main() {
+    setlocale(0, "rus");
+    SECURITY_ATTRIBUTES sa;
+    sa.nLength = sizeof(SECURITY_ATTRIBUTES);
+    sa.lpSecurityDescriptor = NULL;
+    sa.bInheritHandle = TRUE; // Дескриптор наследуемый
+
+    wchar_t fullPath[MAX_PATH];
+    GetFullPathName(L"test.txt", MAX_PATH, fullPath, NULL);
+
+    HANDLE hFile = CreateFile(
+        fullPath, GENERIC_WRITE | GENERIC_READ,
+        FILE_SHARE_READ | FILE_SHARE_WRITE, &sa,
+        CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL
+    );
+
+    if (hFile == INVALID_HANDLE_VALUE) {
+        std::cerr << "Ошибка создания файла: " << GetLastError() << "\n";
+        return 1;
+    }
+
+    const char* data = "Hello from Parent!";
+    DWORD written;
+    WriteFile(hFile, data, strlen(data), &written, NULL);
+
+    FlushFileBuffers(hFile);
+    CloseHandle(hFile);
+
+    wchar_t cmdLine[MAX_PATH];
+    swprintf(cmdLine, MAX_PATH, L"doch_process.exe %s", fullPath);
+
+    STARTUPINFO si = { sizeof(STARTUPINFO) };
+    PROCESS_INFORMATION pi;
+
+    if (!CreateProcess(NULL, cmdLine, NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi)) {
+        std::cerr << "Ошибка запуска дочернего процесса: " << GetLastError() << "\n";
+        return 1;
+    }
+
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+    return 0;
+}
